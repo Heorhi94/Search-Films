9cf00c22fef624498a87bc7b47542b38e1f350c5
heorh <heorh@HEORHI-YAKABSON>
2023-05-04 09:34:32 +0200

    I can't figure out the error yet

---
+++ Movies/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using IMDbApiLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Movies
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttom_Click(object sender, RoutedEventArgs e)
+        {
+            var api = new ApiLib("k_y76947gh");
+            var result = await api.SearchMovieAsync(searc.Text);
+
+            list.ItemsSource = result.Results;
+            list.DisplayMemberPath = "Title";
+            list.SelectedValuePath = "Id";
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using IMDbApiLib;
-using Newtonsoft.Json.Linq;
+﻿
+
+using IMDbApiLib;
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
 namespace WpfApp1
@@ -14,54 +19,189 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //k_hye43l7u
+        //k_y76947gh
+        //k_eknpnu11
+        private readonly ApiLib _api = new ApiLib("k_eknpnu11");
+        private Task _currentTask;
+        private CancellationTokenSource _cancellationTokenSource;
+       
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-
-
         private async void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            var api = new ApiLib("k_y76947gh");
-            var result = await api.SearchTitleAsync(SearchTextBox.Text);
+            try
+            {
+                
+                _cancellationTokenSource?.Cancel();
+                _currentTask?.Wait(); 
 
-            ResultsListBox.ItemsSource = result.Results;
-            ResultsListBox.DisplayMemberPath = "Title";
-            ResultsListBox.SelectedValuePath = "Id";
+                // Clear UI elements
+                resultsListBox.ItemsSource = null;
+                titleLabel.Content = "";
+                image.Source = null;
+                progressBar.Value = 0;
 
+                var result = await _api.SearchMovieAsync(searchTextBox.Text);
+                var movies = result.Results.Select(r => new Movie { Id = r.Id, Title = r.Title }).ToList();
+                resultsListBox.ItemsSource = movies;
+                resultsListBox.DisplayMemberPath = "Title";
+                resultsListBox.SelectedValuePath = "Id";
+                progressBar.Maximum = result.Results.Count;
+                // Process results
+                _cancellationTokenSource = new CancellationTokenSource();
+                _currentTask = ProcessSearchResultsAsync(movies, _cancellationTokenSource.Token);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
 
-        private async Task<List<string>> SearchMovies(string searchTerm)
+        private async void resultsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync($"http://www.omdbapi.com/?s={searchTerm}&apikey=yourapikey");
-            var results = new List<string>();
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var responseString = await response.Content.ReadAsStringAsync();
-                var data = JObject.Parse(responseString);
-                var searchResults = data["Search"];
-                foreach (var result in searchResults)
+                // Cancel any previous task
+                _cancellationTokenSource?.Cancel();
+
+                // Get movie details and display them
+                if (resultsListBox.SelectedValue != null)
                 {
-                    var title = result["Title"].ToString();
-                    results.Add(title);
+                    var id = resultsListBox.SelectedValue.ToString();
+                    var titleData = await _api.TitleAsync(id);
+                    var bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(titleData.Image);
+                    bitmap.EndInit();
+                    image.Source = bitmap;
+                    titleLabel.Content = titleData.FullTitle;
                 }
             }
-            return results;
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
 
-        private async void ResultsListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+
+        private async Task ProcessSearchResultsAsync(List<Movie> movies, CancellationToken cancellationToken)
         {
-            var api = new ApiLib("k_y76947gh");
-            string id = ResultsListBox.SelectedValue.ToString();
-            var titleData = await api.TitleAsync(id);
-            BitmapImage bitmap = new BitmapImage();
+            // Set up progress reporting
+            var progress = new Progress<int>(value =>
+            {
+                progressBar.Value = value;
+                infTask.Content = $"Processing {value}/{movies.Count} movies...";
+            });
+
+            try
+            {
+                int i = 0;
+                while (i < movies.Count && i < 25)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    var movie = movies[i];
+                    Task loadTask = LoadMovieAsync(movie);
+
+                    // Add item to list box when image loaded
+                    await loadTask.ContinueWith(async t =>
+                    {
+                        await Task.Delay(1000);
+                        Dispatcher.Invoke(() =>
+                        {
+                            ((IList<Movie>)resultsListBox.ItemsSource).Add(movie);
+                        });
+                    }, cancellationToken);
+
+
+                    i++;
+                    ((IProgress<int>)progress).Report(i);
+
+                    // Delay for 1 second
+                    await Task.Delay(1000);
+                }
+
+                infTask.Content = "Processing complete.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        private async Task LoadMovieAsync(Movie movie)
+        {
+            var titleData = await _api.TitleAsync(movie.Id);
+            var bitmap = new BitmapImage();
             bitmap.BeginInit();
             bitmap.UriSource = new Uri(titleData.Image);
             bitmap.EndInit();
-            image.Source = bitmap;
-            titleLabel.Content = titleData.FullTitle;
+            movie.IMG = bitmap;
+        }
+
+
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Cancel current task
+            _cancellationTokenSource?.Cancel();
+        }
+
+        private async void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+
+            // Stop loading items into list box
+            _cancellationTokenSource = new();
+        }
+
+        private void resumeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentTask == null || _currentTask.IsCompleted)
+            {
+                // Clear UI elements
+                resultsListBox.ItemsSource = null;
+                titleLabel.Content = "";
+                image.Source = null;
+                progressBar.Value = 0;
+
+                var result = _api.SearchMovieAsync(searchTextBox.Text).Result;
+                var movies = result.Results.Select(r => new Movie { Id = r.Id, Title = r.Title }).ToList();
+                resultsListBox.ItemsSource = movies;
+                resultsListBox.DisplayMemberPath = "Title";
+                resultsListBox.SelectedValuePath = "Id";
+                progressBar.Maximum = result.Results.Count;
+
+                _cancellationTokenSource = new CancellationTokenSource();
+                _currentTask = ProcessSearchResultsAsync(movies, _cancellationTokenSource.Token);
+            }
         }
     }
+
+    public class SearchMovieResult
+    {
+        public int TotalResults { get; set; }
+        public List<Movie> Results { get; set; }
+    }
+
+    public class Movie
+
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        // другие свойства фильма
+        public BitmapImage IMG { get; set; }
+
+    }
+
 }
+
