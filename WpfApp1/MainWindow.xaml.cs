8c212b9f3311534d7cd034bf68af32e8dc11617f
heorh <heorh@HEORHI-YAKABSON>
2023-05-07 11:40:12 +0200

    problem add ListBox

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
-﻿
-
-using IMDbApiLib;
+﻿using IMDbApiLib;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,14 +13,13 @@ using System.Windows.Media.Imaging;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+  
     public partial class MainWindow : Window
     {
         //k_hye43l7u
         //k_y76947gh
         //k_eknpnu11
+        //k_lv26p9yv
         private readonly ApiLib _api = new ApiLib("k_eknpnu11");
         private Task _currentTask;
         private CancellationTokenSource _cancellationTokenSource;
@@ -36,11 +34,9 @@ namespace WpfApp1
         {
             try
             {
-                
                 _cancellationTokenSource?.Cancel();
-                _currentTask?.Wait(); 
+                _currentTask?.Wait();
 
-                // Clear UI elements
                 resultsListBox.ItemsSource = null;
                 titleLabel.Content = "";
                 image.Source = null;
@@ -51,8 +47,8 @@ namespace WpfApp1
                 resultsListBox.ItemsSource = movies;
                 resultsListBox.DisplayMemberPath = "Title";
                 resultsListBox.SelectedValuePath = "Id";
-                progressBar.Maximum = result.Results.Count;
-                // Process results
+                progressBar.Maximum = movies.Count;
+
                 _cancellationTokenSource = new CancellationTokenSource();
                 _currentTask = ProcessSearchResultsAsync(movies, _cancellationTokenSource.Token);
             }
@@ -62,14 +58,68 @@ namespace WpfApp1
             }
         }
 
+        private async Task ProcessSearchResultsAsync(List<Movie> movies, CancellationToken cancellationToken)
+        {
+            var progress = new Progress<int>(value =>
+            {
+                progressBar.Value = value;
+                infTask.Content = $"Processing {value}/{movies.Count} movies...";
+            });
+
+            try
+            {
+                for (int i = 0; i < movies.Count; i++)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    var movie = movies[i];
+                    await LoadMovieAsync(movie);
+
+                    ((IProgress<int>)progress).Report(i + 1);
+
+                    await Task.Delay(1000);
+                }
+
+                infTask.Content = "Processing complete.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private async Task LoadMovieAsync(Movie movie)
+        {
+            var titleData = await _api.TitleAsync(movie.Id);
+            var bitmap = new BitmapImage();
+            if (titleData.Image != null)
+            {
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(titleData.Image);
+                bitmap.EndInit();
+            }
+            else
+            {
+                bitmap = new BitmapImage(new Uri("https://dummyimage.com/100x100/000000/ffffff.png&text=No+Image"));
+            }
+            movie.IMG = bitmap;
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+            MessageBox.Show("Stop loading");
+        }
+
+
         private async void resultsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
             {
-                // Cancel any previous task
                 _cancellationTokenSource?.Cancel();
-
-                // Get movie details and display them
                 if (resultsListBox.SelectedValue != null)
                 {
                     var id = resultsListBox.SelectedValue.ToString();
@@ -89,102 +139,6 @@ namespace WpfApp1
         }
 
 
-        private async Task ProcessSearchResultsAsync(List<Movie> movies, CancellationToken cancellationToken)
-        {
-            // Set up progress reporting
-            var progress = new Progress<int>(value =>
-            {
-                progressBar.Value = value;
-                infTask.Content = $"Processing {value}/{movies.Count} movies...";
-            });
-
-            try
-            {
-                int i = 0;
-                while (i < movies.Count && i < 25)
-                {
-                    if (cancellationToken.IsCancellationRequested)
-                    {
-                        break;
-                    }
-
-                    var movie = movies[i];
-                    Task loadTask = LoadMovieAsync(movie);
-
-                    // Add item to list box when image loaded
-                    await loadTask.ContinueWith(async t =>
-                    {
-                        await Task.Delay(1000);
-                        Dispatcher.Invoke(() =>
-                        {
-                            ((IList<Movie>)resultsListBox.ItemsSource).Add(movie);
-                        });
-                    }, cancellationToken);
-
-
-                    i++;
-                    ((IProgress<int>)progress).Report(i);
-
-                    // Delay for 1 second
-                    await Task.Delay(1000);
-                }
-
-                infTask.Content = "Processing complete.";
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}");
-            }
-        }
-
-
-        private async Task LoadMovieAsync(Movie movie)
-        {
-            var titleData = await _api.TitleAsync(movie.Id);
-            var bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.UriSource = new Uri(titleData.Image);
-            bitmap.EndInit();
-            movie.IMG = bitmap;
-        }
-
-
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Cancel current task
-            _cancellationTokenSource?.Cancel();
-        }
-
-        private async void stopButton_Click(object sender, RoutedEventArgs e)
-        {
-            _cancellationTokenSource?.Cancel();
-
-            // Stop loading items into list box
-            _cancellationTokenSource = new();
-        }
-
-        private void resumeButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (_currentTask == null || _currentTask.IsCompleted)
-            {
-                // Clear UI elements
-                resultsListBox.ItemsSource = null;
-                titleLabel.Content = "";
-                image.Source = null;
-                progressBar.Value = 0;
-
-                var result = _api.SearchMovieAsync(searchTextBox.Text).Result;
-                var movies = result.Results.Select(r => new Movie { Id = r.Id, Title = r.Title }).ToList();
-                resultsListBox.ItemsSource = movies;
-                resultsListBox.DisplayMemberPath = "Title";
-                resultsListBox.SelectedValuePath = "Id";
-                progressBar.Maximum = result.Results.Count;
-
-                _cancellationTokenSource = new CancellationTokenSource();
-                _currentTask = ProcessSearchResultsAsync(movies, _cancellationTokenSource.Token);
-            }
-        }
     }
 
     public class SearchMovieResult
@@ -198,7 +152,6 @@ namespace WpfApp1
     {
         public string Id { get; set; }
         public string Title { get; set; }
-        // другие свойства фильма
         public BitmapImage IMG { get; set; }
 
     }
