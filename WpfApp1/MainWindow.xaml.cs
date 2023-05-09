28f09b6899a88a5dfcbd3cc2d77c0a297bb980d6
heorh <heorh@HEORHI-YAKABSON>
2023-05-09 14:17:17 +0200

    everything works, I'll try to do it through MVVM

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,22 +16,32 @@ namespace WpfApp1
   
     public partial class MainWindow : Window
     {
-        //k_hye43l7u
-        //k_y76947gh
-        //k_eknpnu11
+        //k_hye43l7u no
+        //k_y76947gh no
+        //k_eknpnu11 no
         //k_lv26p9yv
-        private readonly ApiLib _api = new ApiLib("k_eknpnu11");
+        private readonly ApiLib _api = new ApiLib("k_lv26p9yv");
         private Task _currentTask;
         private CancellationTokenSource _cancellationTokenSource;
-       
+        List<Movie> movies;
+        List<Movie> downloads;
+        private int currentMovieIndex = 0;
+
+
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+
+        private async void searchButton_Click(object sender, RoutedEventArgs e)
         {
+
+            if (movies != null)
+            {
+                movies.Clear();
+            }
             try
             {
                 _cancellationTokenSource?.Cancel();
@@ -43,14 +53,16 @@ namespace WpfApp1
                 progressBar.Value = 0;
 
                 var result = await _api.SearchMovieAsync(searchTextBox.Text);
-                var movies = result.Results.Select(r => new Movie { Id = r.Id, Title = r.Title }).ToList();
+                movies = result.Results.Select(r => new Movie { Id = r.Id, Title = r.Title, IMG = r.Image }).ToList();
+                downloads = new List<Movie>(); // Инициализация списка downloads
                 resultsListBox.ItemsSource = movies;
                 resultsListBox.DisplayMemberPath = "Title";
                 resultsListBox.SelectedValuePath = "Id";
+                progressBar.Minimum = 0;
                 progressBar.Maximum = movies.Count;
 
                 _cancellationTokenSource = new CancellationTokenSource();
-                _currentTask = ProcessSearchResultsAsync(movies, _cancellationTokenSource.Token);
+                _currentTask = ProcessSearchResultsAsync(_cancellationTokenSource.Token);
             }
             catch (Exception ex)
             {
@@ -58,7 +70,8 @@ namespace WpfApp1
             }
         }
 
-        private async Task ProcessSearchResultsAsync(List<Movie> movies, CancellationToken cancellationToken)
+
+        private async Task ProcessSearchResultsAsync(CancellationToken cancellationToken)
         {
             var progress = new Progress<int>(value =>
             {
@@ -68,28 +81,30 @@ namespace WpfApp1
 
             try
             {
-                for (int i = 0; i < movies.Count; i++)
+                for (int i = currentMovieIndex; i < movies.Count; i++)
                 {
+                    downloads.Add(movies[i]); // Добавить загруженный ранее элемент в список downloads
+                    ((IProgress<int>)progress).Report(i + 1);
+                    await LoadMovieAsync(downloads[i]);
+                    await Task.Delay(1000);
+                    currentMovieIndex = i + 1;
                     if (cancellationToken.IsCancellationRequested)
                     {
                         break;
                     }
-
-                    var movie = movies[i];
-                    await LoadMovieAsync(movie);
-
-                    ((IProgress<int>)progress).Report(i + 1);
-
-                    await Task.Delay(1000);
                 }
 
-                infTask.Content = "Processing complete.";
+                infTask.Content = $"Processing paused. Downloaded {currentMovieIndex}/{movies.Count} movies";
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"An error occurred: {ex.Message}");
             }
         }
+
+
+
+
 
         private async Task LoadMovieAsync(Movie movie)
         {
@@ -101,17 +116,20 @@ namespace WpfApp1
                 bitmap.UriSource = new Uri(titleData.Image);
                 bitmap.EndInit();
             }
+
             else
             {
                 bitmap = new BitmapImage(new Uri("https://dummyimage.com/100x100/000000/ffffff.png&text=No+Image"));
             }
-            movie.IMG = bitmap;
+            movie.Image = bitmap;
+            progressBar.Dispatcher.Invoke(() => progressBar.Value++);
+            titleLabel.Content = titleData.FullTitle;
         }
 
-        private void stopButton_Click(object sender, RoutedEventArgs e)
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
         {
-            _cancellationTokenSource?.Cancel();
-            MessageBox.Show("Stop loading");
+            _cancellationTokenSource.Cancel();
+            MessageBox.Show("Pause loading");
         }
 
 
@@ -119,15 +137,23 @@ namespace WpfApp1
         {
             try
             {
-                _cancellationTokenSource?.Cancel();
-                if (resultsListBox.SelectedValue != null)
+                if (resultsListBox.SelectedIndex != -1)
                 {
-                    var id = resultsListBox.SelectedValue.ToString();
+                    int res = resultsListBox.SelectedIndex;
+                    var id = downloads[res].Id;
                     var titleData = await _api.TitleAsync(id);
                     var bitmap = new BitmapImage();
-                    bitmap.BeginInit();
-                    bitmap.UriSource = new Uri(titleData.Image);
-                    bitmap.EndInit();
+                    if (titleData.Image != null)
+                    {
+                        bitmap.BeginInit();
+                        bitmap.UriSource = new Uri(downloads[res].IMG);
+                        bitmap.EndInit();
+                    }
+
+                    else
+                    {
+                        bitmap = new BitmapImage(new Uri("https://dummyimage.com/100x100/000000/ffffff.png&text=No+Image"));
+                    }
                     image.Source = bitmap;
                     titleLabel.Content = titleData.FullTitle;
                 }
@@ -138,21 +164,56 @@ namespace WpfApp1
             }
         }
 
+        private void continueButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_cancellationTokenSource != null && _cancellationTokenSource.IsCancellationRequested)
+            {
+                _cancellationTokenSource = new CancellationTokenSource();
+                progressBar.Minimum = currentMovieIndex; // Установите минимальное значение progressBar на сохраненной позиции
+                progressBar.Maximum = movies.Count; // Обновите максимальное значение progressBar
+                _currentTask = ProcessSearchResultsAsync(_cancellationTokenSource.Token);
+            }
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_cancellationTokenSource != null && _cancellationTokenSource.Token.CanBeCanceled)
+            {
+                _cancellationTokenSource.Cancel();
+            }
+            _cancellationTokenSource?.Dispose();
+            _cancellationTokenSource = null;
+
+     /*       // Wyczyść listy i zresetuj indeks filmu
+            movies.Clear();
+            downloads.Clear();
+            currentMovieIndex = 0;
+
+            // Wyczyść wyświetlane informacje
+            resultsListBox.ItemsSource = null;
+            titleLabel.Content = "";
+            image.Source = null;
+            progressBar.Value = 0;*/
+
+            // Zresetuj zadanie aktualnego procesu
+            _currentTask = null;
+
+            MessageBox.Show("Loading stopped");
+            infTask.Content = "Searc stopping";
+        }
 
     }
 
-    public class SearchMovieResult
-    {
-        public int TotalResults { get; set; }
-        public List<Movie> Results { get; set; }
-    }
+
 
     public class Movie
 
     {
         public string Id { get; set; }
         public string Title { get; set; }
-        public BitmapImage IMG { get; set; }
+        public string IMG { get; set; }
+
+        public BitmapImage Image { get; set; }
 
     }
 
